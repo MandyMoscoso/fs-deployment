2bde79c5bfa29de97e56fa44544cea1186cacba4
Mandy <quyenmoscoso@gmail.com>
2022-08-31 14:37:17 -0500

    test 4

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, '../static')));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
